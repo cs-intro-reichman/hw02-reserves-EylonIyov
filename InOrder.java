0ebf48efd15248c6cf085765e20ad676a994f4db
EylonIyov <eylon.iyov@runi.ac.il>
2024-02-01 16:06:35 +0200

    fixed Damka Board

--- InOrder.java
+++ InOrder.java
@@ -5,12 +5,13 @@
 public class InOrder {
 	public static void main (String[] args) {
 		int numOld=0, numNew;
+		String order = "";
 		numNew = (int)(Math.random()*10);
 		while (numNew >= numOld){
-			System.out.print(numNew+ " ");
+			order += numNew+ " ";
 			numOld = numNew;
 			numNew = (int)(Math.random()*10);
 		}
-		
+		System.out.println(order);
 	}
 }
